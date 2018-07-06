a2ccb3309ed59f6e2ab96ec811cbcc3f5637e0a0
Joe <joetho@zillowgroup.com>
2018-07-06 11:43:05 -0700

    Update comps ui

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   div#header__container
     #header__title CompsTool
+    .divider
+    .inp__label Find Comps to:
     VueGoogleAutocomplete.inp.inp__long(
       ref="address",
       id="place",
@@ -16,8 +18,9 @@
     .inp__label ±
     input.inp(placeholder="$$$", :options="[]", :searchable="false", value="10000", v-model="a.pDelta")
     .divider
-    .inp__label Distance within ±
-    input.inp(placeholder="n miles", :options="[]", :searchable="false", value="1", v-model="a.distance")
+    .inp__label Distance within
+    input.inp(placeholder="n miles", :options="[]", :searchable="false", value="1", v-model="a.radius")
+    .inp__label Miles
 </template>
 <script>
 import Multiselect from 'vue-multiselect'
@@ -33,6 +36,42 @@ export default {
   data () {
     VueGoogleMaps.loaded.then(() => {
       window.google = VueGoogleMaps.gmapApi();
+
+google.maps.InfoWindowZ=function(opts){
+          var GM = google.maps,
+              GE = GM.event,
+              iw = new GM.InfoWindow(),
+              ce;
+
+             if(!GM.InfoWindowZZ){
+                GM.InfoWindowZZ=Number(GM.Marker.MAX_ZINDEX);
+             }
+
+             GE.addListener(iw,'content_changed',function(){
+               if(typeof this.getContent()=='string'){
+                  var n=document.createElement('div');
+                      n.innerHTML=this.getContent();
+                      this.setContent(n);
+                      return;
+               }
+               GE.addListener(this,'domready',
+                               function(){
+                                var _this=this;
+                                _this.setZIndex(++GM.InfoWindowZZ);
+                                if(ce){
+                                  GM.event.removeListener(ce);
+                                }
+                                ce=GE.addDomListener(this.getContent().parentNode
+                                                  .parentNode.parentNode,'click',
+                                                  function(){
+                                                  _this.setZIndex(++GM.InfoWindowZZ);
+                                });
+                              })
+             });
+
+             if(opts)iw.setOptions(opts);
+             return iw;
+        }
       this.enabled = true;
     });
 
@@ -106,7 +145,7 @@ export default {
   }
   .inp__label {
     padding: 10px;
-    margin: 5px 15px;
+    margin: 5px 0px;
     float: left;
     color: #FFF;
     font-family: Nunito;
@@ -116,6 +155,7 @@ export default {
     height: 100%;
     background-color: rgba(255,255,255,0.2);
     width: 1px;
+    margin: 0 10px;
     float: left;
   }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,69 +17,101 @@
     components: {
       Header
     },
-    mounted () {
-      this.mapObj = map.__vue__.$mapObject.set
-    },
     data () {
       return {
+        lastUpdate: null,
+        queryTimeout: null,
         mapObj: null,
         markers: [],
         position: {lat:33.4484, lng:-112.0740},
         selected: {
-          radius: 0,
+          radius: 1,
           price: 0,
-          pDelta: 0
+          pDelta: 0,
         }
       }
     },
     methods: {
       update (loc) {
-        // Update the housing markers and fetch new ones.
-        this.query(loc);
-        this.position = loc;
+        // Clear the markers if the position has changed.
+        if (loc.lat !== this.position.lat || loc.lng !== this.position.lng) {
+          this.clearMarkers()
+        }
 
-        // Update the map
-        this.$refs.map.$mapPromise.then((map) => {
-          map.panTo(loc)
-          map.setZoom(18)
+        this.position = loc
+
+        clearTimeout(this.queryTimeout)
+
+        this.queryTimeout = setTimeout(() => {
+          this.lastUpdate = Date.now()
+          // Update the housing markers and fetch new ones.
+          this.query(this.lastUpdate, 0);
+
+          // Update the map
+          this.$refs.map.$mapPromise.then((map) => {
+            map.panTo(loc)
+            map.setZoom(16)
+          })
+        }, 1000)
+      },
+      clearMarkers () {
+        this.markers.forEach(marker => {
+          marker.setMap(null)
         })
       },
       updateMarkers (bundle) {
-        bundle.slice(0,1).forEach(item => {
+        bundle.forEach(item => {
           const price = item.ClosePrice || 0
           const priceStr = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
-          ({
-            full: item,
-            lat: item.Coordinates[0],
-            lng: item.Coordinates[1],
-            price: price,
-            contentStr: priceStr
+          const lat = item.Coordinates[1]
+          const lng = item.Coordinates[0]
+
+          let info = new google.maps.InfoWindowZ({
+            position: new google.maps.LatLng(lat, lng),
+            content: priceStr
           })
+
+          info.setMap(this.$refs.map.$mapObject)
+          this.markers.push(info)
         })
       },
-      query (loc) {
-        let {distance, price, pDelta} = this.selected
+      query (time, offset) {
+        let {radius, price, pDelta} = this.selected
+        const loc = this.position
 
         price = parseInt(price)
         pDelta = parseInt(pDelta)
+        offset = offset || 0
+
+        // Gaurd against fetching after change in address
+        if (this.lastUpdate !== time) {
+          return
+        }
 
         this.$http.get([
           'https://rets.io/api/v2/armls/listings',
           '?access_token=71356a24f85ff5b0d977ba18bf56f2ff',
-          `&near=${loc.lng},${loc.lat}&radius=${distance}mi`,
+          `&near=${loc.lng},${loc.lat}&radius=${radius}mi`,
           `&ClosePrice[gt]=${price-pDelta}`,
-          `&ClosePrice[lt]=${price+pDelta}`
+          `&ClosePrice[lt]=${price+pDelta}`,
+          `&offset=${offset}`
         ].join(''))
-          .then(a => {
-            this.updateMarkers(a.body.bundle);
+          .then(resp => {
+            const {body} = resp
+            this.updateMarkers(body.bundle);
+
+            // Keep finding stuff if there's more to find.
+            if ((offset + body.bundle.length) !== body.total) {
+              this.query(time, body.bundle.length + offset)
+            }
           })
       }
     },
     watch: {
       selected: {
         handler () {
-          this.query(this.position)
+          this.update(this.position);
         },
         deep: true
       }
