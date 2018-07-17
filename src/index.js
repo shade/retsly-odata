e01b352b3fa8e1bd0fa82708c6b72a056d19d832
Joe <joetho@zillowgroup.com>
2018-07-17 13:59:35 -0700

    Add endpoints with IDs to the api

--- src/index.js
+++ src/index.js
@@ -11,43 +11,28 @@ const TEST_DATASET = 'test'
 const TEST_TOKEN = '6baca547742c6f96a6ff71b138424f21'
 
 class RetslyOData {
-  /**
-   * Initializes a Retsly OData JS SDK
-   * @param {String} token - The provided Auth token
-   * @param {Object} opts - An object of options
-   */
-  constructor (token, dataset, endpoint, opts) {
-    // Set default options.
-    this._dataset = TEST_DATASET
-    this.urlBase = '/api/v2/OData'
-    // Make sure the token is specified
+
+  constructor (token, vendor) {
+    if (!vendor) {
+      vendor = 'test'
+    }
+
     if (!token) {
-      const msg = 'You must provide a browser token e.g. `new Retsly(token, endpoint)`'
-      throw new Error(msg)
+      throw new Error('Please provide a token. Otherwise, use the test token via Retsly.TEST_TOKEN')
     }
+  }
 
-    // Make sure an endpoint is set, and valid.
-    if (!endpoint) {
-      const msg = 'You must provide an endpoint e.g. `new Retsly(token, endpoint)`'
-      throw new Error(msg)
-    }
-    if (!VALID_ENDPOINTS.includes(endpoint)) {
-      let msg = `${endpoint} is not a valid endpoint. Choose one of: ${JSON.stringify(VALID_ENDPOINTS)}`
-
-      throw new Error(msg)
-    }
-
-    // Make sure all the options are previously specified
-    for (var opt in opts) {
-      if (!DEFAULT_OPTIONS.hasOwnProperty(opts)) {
-        const msg = `[${opt}] is not a valid option.`
-        throw new Error(msg)
-      }
-    }
-
-    Object.assign(this, opts)
-    this._token = token
-    this.query = {}
+  Property (key) {
+    this.endpoint = `Property${key?`(${key})`:''}`
+  }
+  Member (key) {
+    this.endpoint = `Member${key?`(${key})`:''}`
+  }
+  Office (key) {
+    this.endpoint = `Office${key?`(${key})`:''}`
+  }
+  OpenHouse (key) {
+    this.endpoint = `OpenHouse${key?`(${key})`:''}`
   }
 }
 
