dca15a4aa26f2c78dcaf999acc4a76ec6a1b5af6
Joe Thomas <joetho@zillowgroup.com>
2018-07-17 11:33:32 -0700

    Create Map.

---
+++ src/components/MapView.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div
+    google-map(
+      id="map",
+      :center="center",
+      :zoom="zoom"
+    )
+</template>
+<script>
+export default {
+  name: 'map-view',
+  data () {
+    return {
+      center: {
+        lat: 49.275201,
+        lng: -123.120775
+      },
+      zoom: 18
+    }
+  }
+}
+</script>
+<style>
+  #map {
+    height: 100%;
+    width: 50%;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+  }
+</style>
+
